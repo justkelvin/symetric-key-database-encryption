83165c84c2a3f6bff2a229b6e22c2e452a46618d
justkelvin <muia.muinde@s.karu.ac.ke>
2022-11-15 21:20:16 +0300

    add main functions

--- main.py
+++ main.py
@@ -8,30 +8,33 @@ import src.app as app
 def main():
     app_instance = app.App()
     while True:
-        print('1. Login')
-        print('2. Register')
-        print('3. Exit')
-        print('4. Reset')
-        choice = input('Enter your choice: ')
-        if choice == '1':
-            email = input('Enter your email: ')
-            password = input('Enter your password: ')
-            app_instance.login(email, password)
-        elif choice == '2':
-            name = input('Enter your name: ')
-            email = input('Enter your email: ')
-            password = input('Enter your password: ')
-            app_instance.insert_data(name, email, password)
-        elif choice == '4':
-            name = input('Enter your name: ')
-            email = input('Enter your email: ')
-            password = input('Enter new password: ')
-            app_instance.reset(name, email, password)
-        elif choice == '3':
+        try:
+            print('1. Login')
+            print('2. Register')
+            print('3. Exit')
+            print('4. Reset')
+            choice = input('Enter your choice: ')
+            if choice == '1':
+                email = input('Enter your email: ')
+                password = input('Enter your password: ')
+                app_instance.login(email, password)
+            elif choice == '2':
+                name = input('Enter your name: ')
+                email = input('Enter your email: ')
+                password = input('Enter your password: ')
+                app_instance.insert_data(name, email, password)
+            elif choice == '4':
+                name = input('Enter your name: ')
+                email = input('Enter your email: ')
+                password = input('Enter new password: ')
+                app_instance.reset(name, email, password)
+            elif choice == '3':
+                sys.exit(0)
+            else:
+                print('Invalid choice')
+        except KeyboardInterrupt:
             sys.exit(0)
-        else:
-            print('Invalid choice')
-
+            
 
     # while 1:
     #     print('1. LOgin')
